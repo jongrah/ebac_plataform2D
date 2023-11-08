cb449d9506c8a0832317107510ead61f2016debe
jongrah <150179348+jongrah@users.noreply.github.com>
2023-11-08 10:27:22 -0300

    create jump mecanic

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -6,11 +6,20 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
+    //public Vector2 velocity;
+    public Vector2 friction = new Vector2 (-.1f, 0);
 
     public float speed;
+    public float forceJump = 2;
 
     private void Update()
+    {
+        HandleJump();
+        HandleMoviment();
+
+    }
+
+    private void HandleMoviment()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -22,9 +31,25 @@ public class Player : MonoBehaviour
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
         }
+
+       if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+       
     }
 
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
+            myRigidbody.velocity = Vector2.up * forceJump;
+        }
 
-
+    }
 }
-
