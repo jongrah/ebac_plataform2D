c8d28c3e627b1688ce7110a0470f744fa08e87fe
jongrah <150179348+jongrah@users.noreply.github.com>
2023-12-26 12:07:00 -0300

    add vfx effects + manager

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -40,6 +40,13 @@ public class Player : MonoBehaviour
 
     [SerializeField] private HeathBase _heathBase;
 
+    [Header("Jump Collision Check")]
+    public Collider2D collider2D;
+    public float distToGround;
+    public float spaceToGround = .1f;
+
+    public ParticleSystem jumpVFX;
+
     private void Awake()
     {
         if (_heathBase != null)
@@ -48,6 +55,17 @@ public class Player : MonoBehaviour
         }
 
         _currentPlayer = Instantiate(soPlayerSetup.player,transform);
+
+        if (collider2D != null)
+        {
+            distToGround = collider2D.bounds.extents.y;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, distToGround + spaceToGround);
+        return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + spaceToGround);
     }
 
     private void OnPlayerKill()
@@ -60,6 +78,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        IsGrounded();
         HandleJump();
         HandleMoviment();
 
@@ -122,7 +141,7 @@ public class Player : MonoBehaviour
 
     private void HandleJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.velocity = Vector2.up * soPlayerSetup.forceJump;
@@ -138,8 +157,15 @@ public class Player : MonoBehaviour
             }
             DOTween.Kill(myRigidbody.transform);
             //HandleScaleJump();
+            PlayJumpVFX();
         }
 
+    }
+
+    private void PlayJumpVFX()
+    {
+        VFXManager.Instance.PlayVFXByType(VFXManager.VFXType.JUMP,transform.position);
+        //if(jumpVFX != null) jumpVFX.Play();
     }
 
     /*private void HandleScaleJump()
